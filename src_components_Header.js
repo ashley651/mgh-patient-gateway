f2026606a7229c55d064179847bb82886aca3018
ashley651 <ashley651jones@outlook.com>
2024-08-23 16:56:49 +0530

    Create src_components_Header.js

---
+++ src_components_Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>MGH Patient Gateway</h1>
+    </header>
+  );
+}
+
+export default Header;
